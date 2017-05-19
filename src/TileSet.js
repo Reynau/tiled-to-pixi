77a7938127cdc1d3c4c84d0f9d0e8c822efa006a
Reynau <xavi.lopez.reynau@gmail.com>
2017-05-19 23:58:06 +0200

    Add TileSet first version

---
+++ src/TileSet.js
@@ -0,0 +1,27 @@
+class TileSet {
+  constructor(route, tileSet) {
+    console.log('TileSet constructor - var route = ' + route)
+    this.setTileSetProperties(tileSet)
+    this.baseTexture = PIXI.Texture.fromImage(route + '/the-game/' + tileSet.image.source, false, PIXI.SCALE_MODES.NEAREST)
+    this.setTileTextures()
+  }
+
+  setTileSetProperties(tileSet) {
+    for (let property in tileSet) {
+      if (tileSet.hasOwnProperty(property)) {
+        this[property] = tileSet[property]
+      }
+    }
+  }
+
+  setTileTextures() {
+    this.textures = []
+    for (let y = this.margin; y < this.image.height; y += this.tileHeight + this.spacing) {
+      for (let x = this.margin; x < this.image.width; x += this.tileWidth + this.spacing) {
+        this.textures.push(new PIXI.Texture(this.baseTexture, new PIXI.Rectangle(x, y, this.tileWidth, this.tileHeight)))
+      }
+    }
+  }
+}
+
+module.exports = TileSet
