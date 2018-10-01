5b5140704cac1d93e3336c92e41014af7a4d1d42
Xavier Lopez Reynau <xavi.lopez.reynau@gmail.com>
2018-10-02 00:25:23 +0200

    Fix CollisionLayer

--- example/viewer/bundle.js
+++ example/viewer/bundle.js
@@ -57949,37 +57949,19 @@ module.exports = class CollisionLayer {
 
   constructor(layer) {
     this.constructCollisionsMap(layer.tiles)
+
     this.width = layer.map.width
     this.height = layer.map.height
+
+    this.tileWidth = layer.map.tileWidth
+    this.tileHeight = layer.map.tileHeight
   }
 
   isWalkable(x, y) {
-    let posx = Math.floor(x / 16)
-    let posy = Math.floor(y / 16)
+    let posx = Math.floor(x / this.tileWidth)
+    let posy = Math.floor(y / this.tileHeight)
 
-    let resx = x % 16
-    let resy = y % 16
-
-    let actual = this.collisionsMap[posx + posy * this.width]
-
-    let right = this.collisionsMap[posx + posy * this.width + 1]
-    let left = this.collisionsMap[posx + posy * this.width - 1]
-    let up = this.collisionsMap[posx + posy * this.width - this.width]
-    let down = this.collisionsMap[posx + posy * this.width + this.width]
-
-    let downRight = this.collisionsMap[posx + posy * this.width + this.width + 1]
-    let downLeft = this.collisionsMap[posx + posy * this.width + this.width - 1]
-    let upRight = this.collisionsMap[posx + posy * this.width - this.width + 1]
-    let upLeft = this.collisionsMap[posx + posy * this.width - this.width - 1]
-
-    let pwl = 6
-    let pwr = 8
-    let phu = 8
-    let phd = 2
-
-    return !(!actual || resx < pwl && !left || resx > pwr && !right || resy > phu && !down || resy < phd && !up ||
-    resy > phu && resx > pwr && !downRight || resy > phu && resx < pwl && !downLeft ||
-    resy < phd && resx > pwr && !upRight || resy < phd && resx < pwl && !upLeft)
+    return this.collisionsMap[posx + posy * this.width]
   }
 
   constructCollisionsMap(tilesMap) {
@@ -58172,8 +58154,7 @@ class TiledMap extends PIXI.Container {
 
     this.setDataProperties(resource.data)
 
-    this.backgroundLayer = this.createBackgroundLayer()
-    this.addLayer(this.backgroundLayer)
+    this.createBackgroundLayer()
 
     this.setDataTileSets(resource.data, route)
     this.setDataLayers(resource.data)
@@ -58192,7 +58173,7 @@ class TiledMap extends PIXI.Container {
     background.beginFill(0x000000, 0)
     background.drawRect(0, 0, this.width * this.tileWidth, this.height * this.tileHeight)
     background.endFill()
-    return background
+    this.addLayer(background)
   }
 
   setDataTileSets (data, route) {
