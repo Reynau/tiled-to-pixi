b1d5e7ba55cb8c5f69ab40e0f5baf68f0f93f45a
Xavier Lopez Reynau <xavi.lopez.reynau@gmail.com>
2020-03-18 10:01:51 +0100

    Improve readability of TileSet class

--- src/TileSet.js
+++ src/TileSet.js
@@ -19,7 +19,8 @@ class TileSet {
     this.textures = []
     for (let y = this.margin; y < this.image.height; y += this.tileHeight + this.spacing) {
       for (let x = this.margin; x < this.image.width; x += this.tileWidth + this.spacing) {
-        this.textures.push(new PIXI.Texture(this.baseTexture, new PIXI.Rectangle(x, y, this.tileWidth, this.tileHeight)))
+        const tileRectangle = new PIXI.Rectangle(x, y, this.tileWidth, this.tileHeight)
+        this.textures.push(new PIXI.Texture(this.baseTexture, tileRectangle))
       }
     }
   }
